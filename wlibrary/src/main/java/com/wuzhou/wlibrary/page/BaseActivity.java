2caef6bdb9fdb750e159fe58a0e873335da3a435
fengyuan <446950883@qq.com>
2017-08-23 15:11:15 +0800

    Signed-off-by: fengyuan <446950883@qq.com>

--- wlibrary/src/main/java/com/wuzhou/wlibrary/page/BaseActivity.java
+++ wlibrary/src/main/java/com/wuzhou/wlibrary/page/BaseActivity.java
@@ -1,15 +1,32 @@
 package com.wuzhou.wlibrary.page;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.wuzhou.xlibrary.R;
 
 public class BaseActivity extends AppCompatActivity {
+    protected String TAG=this.getClass().getSimpleName();
+    protected Activity mActivity;
+    protected String user_id;
+
+    public FrameLayout f_content;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+        super.setContentView(R.layout.activity_base);
+        mActivity=this;
+        f_content= (FrameLayout) findViewById(R.id.f_content);
     }
 
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        f_content.removeAllViews();
+        View.inflate(this,layoutResID,f_content);
+        onContentChanged();
+    }
 }
--- wlibrary/src/main/java/com/wuzhou/wlibrary/page/TitleActivity.java
+++ wlibrary/src/main/java/com/wuzhou/wlibrary/page/TitleActivity.java
@@ -1,16 +1,78 @@
 package com.wuzhou.wlibrary.page;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.wuzhou.xlibrary.R;
 
 public class TitleActivity extends BaseActivity {
-    Toolbar toolbar;
+    protected Toolbar toolbar;
+    protected FrameLayout t_content;
+    protected ImageView imv_back;
+    protected TextView tv_title;
+    protected ImageView imv_right;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_title);
+        super.setContentView(R.layout.activity_title);
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        t_content= (FrameLayout) findViewById(R.id.t_content);
+        imv_back= (ImageView) findViewById(R.id.imv_back);
+        tv_title= (TextView) findViewById(R.id.tv_title);
+        imv_right= (ImageView) findViewById(R.id.imv_right);
+        showBack(true);
+        imv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    protected void showBack(boolean show){
+        if(imv_back!=null){
+            if(show){
+                imv_back.setVisibility(View.VISIBLE);
+            }else{
+                imv_back.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    protected void showTitleRight(boolean show){
+        if(imv_right!=null){
+            if(show){
+                imv_right.setVisibility(View.VISIBLE);
+                imv_right.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onTitleRight();
+                    }
+                });
+            }else{
+                imv_right.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    protected void onTitleRight(){
+
+    }
+
+    protected void setTitle(String name){
+        if(tv_title!=null)tv_title.setText(name);
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        t_content.removeAllViews();
+        View.inflate(this,layoutResID,t_content);
+        onContentChanged();
     }
 
 
