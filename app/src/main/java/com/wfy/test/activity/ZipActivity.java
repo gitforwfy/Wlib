80c931db94d77fdc69f65e2751f70cd7530a5af0
fengyuan <446950883@qq.com>
2017-08-24 16:49:22 +0800

    Signed-off-by: fengyuan <446950883@qq.com>

--- app/src/main/java/com/wfy/test/activity/ZipActivity.java
+++ app/src/main/java/com/wfy/test/activity/ZipActivity.java
@@ -11,5 +11,6 @@ public class ZipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zip_activity);
+        setTitle("下载解压示例");
     }
 }
