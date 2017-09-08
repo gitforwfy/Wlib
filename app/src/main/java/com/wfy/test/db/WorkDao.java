12ac9452939f865c23fb8a90dc7876b977873172
fengyuan <446950883@qq.com>
2017-09-08 15:47:08 +0800

    Signed-off-by: fengyuan <446950883@qq.com>

--- app/src/main/java/com/wfy/test/db/WorkDao.java
+++ app/src/main/java/com/wfy/test/db/WorkDao.java
@@ -14,4 +14,20 @@ public class WorkDao extends BaseDaoImpl {
     public WorkDao(Context context) {
         super(context, Worker.class);
     }
+
+    /**
+     * 自定义方法，常用的话 为了方便
+     * @param worker
+     * @return
+     */
+    public boolean isExist(Worker worker){
+        boolean b;
+        if(query("jobNum",worker.jobNum)==null){
+            b=false;
+        }else{
+            b=true;
+        }
+        return b;
+    }
+
 }
